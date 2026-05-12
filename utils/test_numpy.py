6a2c05a296a730fc9324a0e2823d26802f96de63
murugan anna <vishalwin@gmail.com>
2026-05-12 14:27:38 +0530

    numpy system

---
+++ utils/test_numpy.py
@@ -0,0 +1,21 @@
+#school wanted  average marks
+# highest marks
+#lowest marks
+
+import numpy as np
+
+marks=np.array([56,67,56,78,98,56])
+
+print ("marks ", marks)
+print ("Average marks ",
+       np.mean(marks))
+print("Highest Marks: ",
+      np.max(marks))
+print(np.min(marks))
+
+# average execution tiem
+#slowest test
+#fastest test
+#ecommerce  track daily sales
+
+# mean , sum, max, min , reshape , array
