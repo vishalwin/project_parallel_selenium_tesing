0f8ddbbe5cca9287ad375e991ccfe4d2f4ddecfd
murugan anna <vishalwin@gmail.com>
2026-05-14 16:02:11 +0530

    add steps import page and add feature fiels code behind

---
+++ features/wait.feature
@@ -0,0 +1,6 @@
+Feature: Dynamic wait demo
+
+Scenario:  Wait for textbox to appear
+Given user opens wait demo page
+When user clicks add textbox1 button
+Then textbox1 should appear
---
+++ pages/wait_page.py
@@ -0,0 +1,21 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
+from selenium.webdriver.support import expected_conditions as EC
+
+class WaitsPage:
+    ADD_TEXTBOX_BTN = (By.ID, "btn1")
+    TEXTBOX1 = (By.ID, "txt1")
+    def __init__(self,driver):
+        self.driver = driver
+        
+    def click_add_textbox(self,driver):
+        self.driver.find_element(*self.ADD_TEXTBOX_BTN).click()
+        
+        #create a wait object
+        wait = WebDriverWait(self.driver,10)
+        
+        #create textbox and add wait to that 
+        textbox= wait.until(EC.visibility_of_element_located(self.TEXTBOX1))
+        
+        return textbox.is_displayed()
